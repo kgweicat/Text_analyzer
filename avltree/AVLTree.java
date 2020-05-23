655de117f418fb0a6e7543992d6128c4b46ea2de
Che-Wei (Joanne) Chou <37817830+kgweicat@users.noreply.github.com>
2020-05-22 21:58:19 -0500

    Update AVLTree.java

--- avltree/AVLTree.java
+++ avltree/AVLTree.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package avltree;
 
 /**
  *
- * @author Che-Wei Chou
+ * @author Che-Wei (Joanne) Chou
  */
 public class AVLTree {
 
